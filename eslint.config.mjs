bc75c4455b22740e8a6c4c6f966c28e37b26e681
Hmtgit7 <hmtloharcoding3579@gmail.com>
2026-05-27 11:00:52 +0530

    chore: add project quality checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,8 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".tmp/**",
+    ".pnpm-store/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
